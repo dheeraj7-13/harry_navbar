9eaecec7304c9dfff4ff12f3659d26d1f67c5390
dheeraj7-13 <86879344+dheeraj7-13@users.noreply.github.com>
2024-05-22 11:36:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+// import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
+console.log("HELLLOOOO");
+
+function App() {
+  return (
+    <> 
+
+    <div className="container">
+<h1>Bonsoir Elliot!</h1>
+<Navbar/>
+<Shop/>
+    </div>
+    </> 
+
+  );
+}
+
+export default App;
---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>     
+      <h2>Buy addidas shoes</h2>
+      <button className="btn btn-primary">+</button>
+        Add this item to cart!
+      <button className="btn btn-primary">-</button>
+</div>
+  )
+}
+
+export default Shop
